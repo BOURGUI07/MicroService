47dad2a8498e4f17e3e89e42e07f0c4374885f22
Youness Bourgui <younessbourgui07@gmail.com>
2024-09-07 10:16:58 +0200

    implement the api gateway

---
+++ gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,39 @@
+package com.example.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/youness/accounts/**")
+						.filters( f -> f.rewritePath("/youness/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/youness/loans/**")
+						.filters( f -> f.rewritePath("/youness/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/youness/cards/**")
+						.filters( f -> f.rewritePath("/youness/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CARDS")).build();
+
+
+	}
+}
